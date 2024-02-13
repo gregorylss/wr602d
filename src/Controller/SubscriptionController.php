e305c5096d7522afd13e1fad6d092ecec5717e00
Grégory LAISSUS <gregory.laissus@etudiant.univ-reims.fr>
2024-02-14 00:16:33 +0100

    feat: fixtures-subs

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscription;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class SubscriptionController extends AbstractController
 {
     #[Route('/subscription', name: 'app_subscription')]
-    public function index(): Response
+    public function index(PersistenceManagerRegistry $doctrine): Response
     {
+
+        $em = $doctrine->getManager();
+
+        $subscription = $em->getRepository(Subscription::class)->findAll();
+
+
+        $user = $this->getUser();
+        $subscriptionUser = $user->getSubscription();
+
+
         return $this->render('subscription/index.html.twig', [
             'controller_name' => 'SubscriptionController',
+            'subscriptions' => $subscription,
+            'subscriptionUser' => $subscriptionUser
         ]);
     }
 }
