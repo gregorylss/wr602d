2dab55795436430ba1e7d9c274681b602a79ec2c
Grégory LAISSUS <gregory.laissus@etudiant.univ-reims.fr>
2024-02-14 01:24:59 +0100

    feat: subs-make-pdf

--- src/Controller/DocController.php
+++ src/Controller/DocController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Pdf;
 use App\Service\SymfonyDocs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 
 class DocController extends AbstractController
 {
@@ -29,12 +31,27 @@ class DocController extends AbstractController
      * @throws TransportExceptionInterface
      */
     #[Route('/pdf/convert', name: 'app_doc_convert', methods: ['POST'])]
-    public function convert(Request $request): Response
+    public function convert(Request $request, PersistenceManagerRegistry $doctrine): Response
     {
+
+
         $url = $request->request->get('url');
         $pdfFilePath = $this->symfonyDocs->generatePdfFromUrl($url);
 
+        // Créer une nouvelle instance de Pdf et lui attribuer les valeurs nécessaires
+        $pdf = new Pdf();
+        $pdf->setTitle('PDF title');
+        $pdf->setCreatedAt(new \DateTimeImmutable());
+        $pdf->setUser($this->getUser()); // Associez le PDF à l'utilisateur actuellement connecté
+
+        // Enregistrez le PDF dans la base de données
+
+       $doctrine->getManager();
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($pdf);
+        $entityManager->flush();
+
+        // Retournez le PDF généré
         return $this->file($pdfFilePath);
     }
-
 }
